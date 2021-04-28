3cc7893f040fc3d7f34999d1c471fe5cbefa02ad
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-04-28 19:37:14 +0600

    sum change

--- src/Component/Home/Advertise/Advertise.js
+++ src/Component/Home/Advertise/Advertise.js
@@ -16,35 +16,35 @@ const Advertise = () => {
                         <p>If your phone has a broken front glass, non-responsive buttons, a broken camera we can fix it in house…</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 col-md-6">
                         <div className="advertise-style p-4 my-5 rounded">
                         <p  className="advertise-icon justify-content-center d-flex align-items-center"><FontAwesomeIcon icon={faMouse} /></p>
                         <h3>Computer Repair</h3>
                         <p>If your phone has a broken front glass, non-responsive buttons, a broken camera we can fix it in house…</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 col-md-6">
                         <div className="advertise-style p-4 my-5 rounded">
                         <p  className="advertise-icon justify-content-center d-flex align-items-center"><FontAwesomeIcon icon={faPersonBooth} /></p>
                         <h3>Electronics Repair</h3>
                         <p>If your phone has a broken front glass, non-responsive buttons, a broken camera we can fix it in house…</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 col-md-6">
                         <div className="advertise-style p-4 my-5 rounded">
                         <p  className="advertise-icon justify-content-center d-flex align-items-center"><FontAwesomeIcon icon={faGamepad} /></p>
                         <h3>Console Repair</h3>
                         <p>If your phone has a broken front glass, non-responsive buttons, a broken camera we can fix it in house…</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 col-md-6">
                         <div className="advertise-style p-4 my-5 rounded">
                         <p  className="advertise-icon justify-content-center d-flex align-items-center"><FontAwesomeIcon icon={faFan} /></p>
                         <h3>Electronics Recovery</h3>
                         <p>If your phone has a broken front glass, non-responsive buttons, a broken camera we can fix it in house…</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-lg-4 col-md-6">
                         <div className="advertise-style p-4 my-5 rounded">
                         <p  className="advertise-icon justify-content-center d-flex align-items-center"><FontAwesomeIcon icon={faLightbulb} /></p>
                         <h3>Check before purchase</h3>
